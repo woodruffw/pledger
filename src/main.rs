50753963476a8c52a606da00e9bcbf1e807305f5
William Woodruff <william@yossarian.net>
2020-08-29 13:16:51 -0400

    treewide: Add an `--all` mode
    This combines all of the ledgers in the specified directory
    into a single stream for analysis. Information about dates
    is lost and replaced with the "*" special sentinel.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use anyhow::Result;
 use chrono::Local;
 use clap::{App, Arg};
 
+use std::path::Path;
 use std::process;
 
 mod pledger;
 
-fn run() -> Result<(), String> {
+fn run() -> Result<()> {
     let date = Local::now().format("%Y-%m").to_string();
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
@@ -33,6 +35,13 @@ fn run() -> Result<(), String> {
                 .multiple(false),
         )
         .arg(
+            Arg::with_name("all")
+                .help("combine all ledgers")
+                .short("a")
+                .long("all")
+                .multiple(false),
+        )
+        .arg(
             Arg::with_name("directory")
                 .help("ledger directory")
                 .index(1)
@@ -42,14 +51,19 @@ fn run() -> Result<(), String> {
         )
         .get_matches();
 
-    let date = pledger::parse_date(matches.value_of("date").unwrap())?;
-    let ledger_dir = matches.value_of("directory").unwrap();
+    let ledger_dir = Path::new(matches.value_of("directory").unwrap());
 
-    if matches.is_present("edit") {
-        return pledger::edit_ledger(&date, &ledger_dir);
-    }
+    let ledger = if matches.is_present("all") {
+        pledger::parse_ledger("*", pledger::read_ledgers(&ledger_dir)?)?
+    } else {
+        let date = pledger::parse_date(matches.value_of("date").unwrap())?;
 
-    let ledger = pledger::parse_ledger(&date, pledger::read_ledger(&ledger_dir, &date)?)?;
+        if matches.is_present("edit") {
+            return pledger::edit_ledger(&date, &ledger_dir);
+        }
+
+        pledger::parse_ledger(&date, pledger::read_ledger(&ledger_dir, &date)?)?
+    };
 
     if matches.is_present("json") {
         println!("{}", serde_json::to_string(&ledger).unwrap());
