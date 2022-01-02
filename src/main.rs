987ea94e76c654bbee33de8f074117e50ec26215
William Woodruff <william@yossarian.net>
2022-01-01 23:24:40 -0500

    Use clap 3.0.0

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ use std::path::Path;
 use std::process;
 
 use anyhow::{anyhow, Result};
-use chrono::{Datelike, Local, Month};
+use chrono::{DateTime, Datelike, Local, Month};
 use clap::{App, Arg, ArgGroup};
+use lazy_static::lazy_static;
 use num_traits::FromPrimitive;
 
 mod pledger;
 
-fn run() -> Result<()> {
-    let now = Local::now();
-    let matches = App::new(env!("CARGO_PKG_NAME"))
+lazy_static! {
+    static ref NOW: DateTime<Local> = Local::now();
+    static ref NOW_FMT: String = NOW.format("%Y-%m").to_string();
+}
+
+fn app() -> App<'static> {
+    App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .group(
@@ -24,14 +29,14 @@ fn run() -> Result<()> {
         )
         .arg(
             Arg::new("all")
-                .about("combine all ledgers")
+                .help("combine all ledgers")
                 .short('a')
                 .long("all")
                 .multiple_occurrences(false),
         )
         .arg(
             Arg::new("year")
-                .about("combine all ledgers from the given year")
+                .help("combine all ledgers from the given year")
                 .short('y')
                 .long("year")
                 .multiple_occurrences(false)
@@ -39,36 +44,36 @@ fn run() -> Result<()> {
         )
         .arg(
             Arg::new("date")
-                .about("use a ledger by date")
+                .help("use a ledger by date")
                 .short('d')
                 .long("date")
                 .multiple_occurrences(false)
-                .default_value(&now.format("%Y-%m").to_string()),
+                .default_value(&NOW_FMT),
         )
         .arg(
             Arg::new("last")
-                .about("use the previous ledger")
+                .help("use the previous ledger")
                 .short('l')
                 .long("last")
                 .multiple_occurrences(false),
         )
         .arg(
             Arg::new("edit")
-                .about("edit the selected ledger")
+                .help("edit the selected ledger")
                 .short('e')
                 .long("edit")
                 .multiple_occurrences(false),
         )
         .arg(
             Arg::new("json")
-                .about("output in JSON format")
+                .help("output in JSON format")
                 .short('j')
                 .long("json")
                 .multiple_occurrences(false),
         )
         .arg(
             Arg::new("filter")
-                .about("produce only ledger entries containing these tags (comma-separated)")
+                .help("produce only ledger entries containing these tags (comma-separated)")
                 .short('f')
                 .long("filter")
                 .multiple_occurrences(false)
@@ -76,13 +81,16 @@ fn run() -> Result<()> {
         )
         .arg(
             Arg::new("directory")
-                .about("ledger directory")
+                .help("ledger directory")
                 .index(1)
                 .required(true)
                 .multiple_occurrences(false)
                 .env("PLEDGER_DIR"),
         )
-        .get_matches();
+}
+
+fn run() -> Result<()> {
+    let matches = app().get_matches();
 
     let ledger_dir = Path::new(matches.value_of("directory").unwrap());
 
@@ -104,11 +112,11 @@ fn run() -> Result<()> {
             pledger::parse_ledger(year, pledger::read_ledgers_for_year(ledger_dir, year)?)?
         }
         (false, false, true, true) => {
-            let last_month = Month::from_u32(now.month())
+            let last_month = Month::from_u32(NOW.month())
                 .ok_or_else(|| {
                     anyhow!(
                         "unlikely failure converting {} into a chrono::Month",
-                        now.month()
+                        NOW.month()
                     )
                 })?
                 .pred();
@@ -118,7 +126,7 @@ fn run() -> Result<()> {
             // NOTE(ww): Without `with_day`, we'd naively jump backyards to an invalid date
             // on some months. For example, July 31st would become June 31st, which isn't a real
             // day. Every month should have a first day, so `with_day(1)` should always succeed.
-            let last = now
+            let last = NOW
                 .with_day(1)
                 .and_then(|d| d.with_month(last_month.number_from_month()))
                 .ok_or_else(|| anyhow!("datetime calculation for the previous month failed"))?;
@@ -172,4 +180,14 @@ fn main() {
             1
         }
     });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_app() {
+        app().debug_assert();
+    }
 }
